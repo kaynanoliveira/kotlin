28906b6c9323ee9268e5e74f9c1533faaa1fc7fd
kaynanoliveira <kaynanoliveira357@gmail.com>
2024-10-22 13:58:29 -0300

    commit

---
+++ 2_controle_de_fluxo/ranges/exemplo1.kt
@@ -0,0 +1,23 @@
+// Intervalos em Kotlin
+
+fun main(){
+for(i in 0..3){          // vai do numero inicial ate o ultimo
+    print(i)
+}
+println(" ")
+
+for(i in 1 until 5){     // vai do numero inicial ate o final e exclui o ultimo.
+    print(i)             
+}
+println(" ")
+
+for(i in 1..10 step 2){  // vai do numero inicial ate o final pulando de 2 em 2.
+    print(i)
+}
+println(" ")
+
+for(i in 5 downTo 0 ){  // vai de trás pra frente 
+    print(i)
+}
+println(" ")
+}
