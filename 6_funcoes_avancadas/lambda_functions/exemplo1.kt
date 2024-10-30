87baebfbc3a43e929cde1fc4a1affcfa701e06bc
kaynanoliveira <kaynanoliveira357@gmail.com>
2024-10-30 15:26:37 -0300

    lambda functions

---
+++ 6_funcoes_avancadas/lambda_functions/exemplo1.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val upperCase1: (String) -> String = {str: String -> str.uppercase() }
+
+    val upperCase2: (String) -> String = {str -> str.uppercase() }
+
+    val upperCase3 = {str: String -> str.uppercase()}
+
+    val upperCase4: (String) -> String = {it.uppercase()}
+
+    val upperCase5: (String) -> String = String::uppercase
+
+    println(upperCase1("hello"))
+    println(upperCase2("hello"))
+    println(upperCase3("hello"))
+    println(upperCase5("hello"))
+}
